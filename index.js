47b9f2d06484cd4d446d192aac5a117b951c90f0
DaniNere <danisilvanere@gmail.com>
2024-08-01 15:34:32 -0300

    :hammer: Envio de Documentação com Swagger

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import { connection, authenticate } from "./src/config/database.js";
 import express from "express";
 import { rotasAluno } from "./src/routers/alunos.js";
+import { readFileSync } from "fs";
+import path from "path";
+
+import swaggerUi from "swagger-ui-express";
 
 authenticate(connection).then (() =>{
     connection.sync();
     });
+    
+const swaggerDocument = JSON.parse(
+      readFileSync(path.resolve('api-docs', 'swagger.json'), 'utf8')
+    );
 
 const app = express()
 app.use(express.json())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(rotasAluno);
 
 app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000/")
+  console.log("Servidor rodando em http://localhost:3000/");
+  console.log('Documentação Swagger disponível em http://localhost:3000/api-docs');
 })
