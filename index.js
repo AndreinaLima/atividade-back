122b737893d9a0360950b1d7c28077d98c8d2798
Andreina Lima <andreinaadelima14@gmail.com>
2024-08-10 17:08:01 -0300

    Ajustes para o deploy no Render

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-import { connection, authenticate } from "./src/config/database.js";
-import express from "express";
-import { rotasAluno } from "./src/routers/alunos.js";
-import { rotasCurso } from "./src/routers/cursos.js";
-import { readFileSync } from "fs";
-import path from "path";
-import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-
-
-authenticate(connection).then (() =>{
-    connection.sync();
-    });
-    
-const swaggerDocument = JSON.parse(
-      readFileSync(path.resolve('api-docs', 'swagger.json'), 'utf8')
-    );
+import { connection, authenticate } from "./src/config/database.js"
+import express from "express"
+import { rotasAluno } from "./src/routers/alunos.js"
+import { rotasCurso } from "./src/routers/cursos.js"
+import { readFileSync } from "fs"
+import path from "path"
+import cors from "cors"
+import swaggerUi from "swagger-ui-express"
 
 const app = express()
+const PORT = process.env.PORT || 3000 // Usa a variável PORT do Render ou 3000 para desenvolvimento
+
+// Testa a conexão com o banco de dados
+authenticate(connection).then(() => {
+  connection.sync()
+})
+
+// Configura o Swagger
+const swaggerDocument = JSON.parse(
+  readFileSync(path.resolve("api-docs", "swagger.json"), "utf8")
+)
+
 app.use(express.json())
+app.use(cors({ origin: "http://localhost:5173" })) // Altere o origin para o domínio da sua aplicação front-end em produção
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(rotasAluno)
+app.use(rotasCurso)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.use(rotasAluno);
-app.use(rotasCurso);
-
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000/");
-  console.log('Documentação Swagger disponível em http://localhost:3000/api-docs');
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}/`)
+  console.log(
+    `Documentação Swagger disponível em http://localhost:${PORT}/api-docs`
+  )
 })
--- src/config/database.js
+++ src/config/database.js
@@ -9,7 +9,7 @@ export const connection = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 3000,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   }
 )
